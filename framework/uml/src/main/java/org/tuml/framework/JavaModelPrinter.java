1cbfb149051ecd2c5cccde27ed4b9f32a4231c4a
pieter <pietermartin@lavabit.com>
2012-08-18 20:07:14 +0200

    do not delete .gitignore on java to text

--- framework/uml/src/main/java/org/tuml/framework/JavaModelPrinter.java
+++ framework/uml/src/main/java/org/tuml/framework/JavaModelPrinter.java
@@ -2,6 +2,7 @@ package org.tuml.framework;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
@@ -22,7 +23,7 @@ public class JavaModelPrinter {
 		if (project.exists()) {
 			File sourceDir = new File(project, SOURCE_FOLDER);
 			try {
-				FileUtils.cleanDirectory(sourceDir);
+				cleanDirectory(sourceDir);
 				for (String name : sourceFiles.keySet()) {
 					String packageName = name.substring(0, name.lastIndexOf(".")).replace(".", "/");
 					File javaPackage = new File(sourceDir + "/" + packageName);
@@ -39,4 +40,38 @@ public class JavaModelPrinter {
 			throw new IllegalStateException(String.format("Project does not exist at %s", project.getAbsolutePath()));
 		}
 	}
+	
+    public static void cleanDirectory(File directory) throws IOException {
+        if (!directory.exists()) {
+            String message = directory + " does not exist";
+            throw new IllegalArgumentException(message);
+        }
+
+        if (!directory.isDirectory()) {
+            String message = directory + " is not a directory";
+            throw new IllegalArgumentException(message);
+        }
+
+        File[] files = directory.listFiles();
+        if (files == null) {  // null if security restricted
+            throw new IOException("Failed to list contents of " + directory);
+        }
+
+        IOException exception = null;
+        for (File file : files) {
+        	if (file.getName().equals(".gitignore")) {
+        		continue;
+        	}
+            try {
+                FileUtils.forceDelete(file);
+            } catch (IOException ioe) {
+                exception = ioe;
+            }
+        }
+
+        if (null != exception) {
+            throw exception;
+        }
+    }
+    
 }
