bbd7c54639f3edaf3c221699493812907bba2528
pieter martin <pietermartin@lavabit.com>
2013-03-02 21:36:37 +0200

    sequence stuff

--- runtime/runtime-domain/src/main/java/org/tuml/runtime/collection/persistent/TinkerOrderedSetImpl.java
+++ runtime/runtime-domain/src/main/java/org/tuml/runtime/collection/persistent/TinkerOrderedSetImpl.java
@@ -3,36 +3,19 @@ package org.tuml.runtime.collection.persistent;
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
-import org.apache.commons.collections.set.ListOrderedSet;
 import org.tuml.runtime.adaptor.GraphDb;
 import org.tuml.runtime.collection.TinkerOrderedSet;
-import org.tuml.runtime.collection.TinkerSequence;
 import org.tuml.runtime.collection.TumlRuntimeProperty;
-import org.tuml.runtime.collection.ocl.BodyExpressionEvaluator;
-import org.tuml.runtime.collection.ocl.BooleanExpressionEvaluator;
-import org.tuml.runtime.collection.ocl.OclStdLibOrderedSet;
-import org.tuml.runtime.collection.ocl.OclStdLibOrderedSetImpl;
 import org.tuml.runtime.domain.TumlNode;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ListIterator;
 
-public class TinkerOrderedSetImpl<E> extends BaseCollection<E> implements TinkerOrderedSet<E> {
+public class TinkerOrderedSetImpl<E> extends TumlBaseOrderedSet<E> implements TinkerOrderedSet<E> {
 
-    protected OclStdLibOrderedSet<E> oclStdLibOrderedSet;
 
     @SuppressWarnings("unchecked")
     public TinkerOrderedSetImpl(TumlNode owner, TumlRuntimeProperty runtimeProperty) {
         super(owner, runtimeProperty);
-        this.internalCollection = new ListOrderedSet();
-        this.oclStdLibOrderedSet = new OclStdLibOrderedSetImpl<E>((ListOrderedSet) this.internalCollection);
-        this.oclStdLibCollection = this.oclStdLibOrderedSet;
-    }
-
-    protected ListOrderedSet getInternalListOrderedSet() {
-        return (ListOrderedSet) this.internalCollection;
     }
 
     @Override
@@ -204,24 +187,6 @@ public class TinkerOrderedSetImpl<E> extends BaseCollection<E> implements Tinker
         }
     }
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public void clear() {
-        maybeLoad();
-        for (E e : new HashSet<E>(this.getInternalListOrderedSet())) {
-            this.remove(e);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public E get(int index) {
-        if (!this.loaded) {
-            loadFromVertex();
-        }
-        return (E) this.getInternalListOrderedSet().get(index);
-    }
-
     @Override
     public E set(int index, E element) {
         E removedElement = this.remove(index);
@@ -229,114 +194,4 @@ public class TinkerOrderedSetImpl<E> extends BaseCollection<E> implements Tinker
         return removedElement;
     }
 
-    @Override
-    public E remove(int index) {
-        E e = this.get(index);
-        this.remove(e);
-        return e;
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        maybeLoad();
-        return this.getInternalListOrderedSet().indexOf(o);
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        throw new RuntimeException("Not supported");
-    }
-
-    @Override
-    public ListIterator<E> listIterator() {
-        throw new RuntimeException("Not supported");
-    }
-
-    @Override
-    public ListIterator<E> listIterator(int index) {
-        throw new RuntimeException("Not supported");
-    }
-
-    @Override
-    public List<E> subList(int fromIndex, int toIndex) {
-        throw new RuntimeException("Not supported");
-    }
-
-    @Override
-    public <R> TinkerSequence<R> collectNested(BodyExpressionEvaluator<R, E> v) {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.collectNested(v);
-    }
-
-    @Override
-    public <T, R> TinkerSequence<T> collect(BodyExpressionEvaluator<R, E> v) {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.collect(v);
-    }
-
-    @Override
-    public <T2> TinkerSequence<T2> flatten() {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.flatten();
-    }
-
-    @Override
-    public TinkerOrderedSet<E> select(BooleanExpressionEvaluator<E> v) {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.select(v);
-    }
-
-    @Override
-    public TinkerOrderedSet<E> append(E e) {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.append(e);
-    }
-
-    @Override
-    public TinkerOrderedSet<E> prepend(E e) {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.prepend(e);
-    }
-
-    @Override
-    public TinkerOrderedSet<E> insertAt(Integer index, E e) {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.insertAt(index, e);
-    }
-
-    @Override
-    public TinkerOrderedSet<E> subOrderedSet(Integer lower, Integer upper) {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.subOrderedSet(lower, upper);
-    }
-
-    @Override
-    public E at(Integer i) {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.at(i);
-    }
-
-    @Override
-    public E first() {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.first();
-    }
-
-    @Override
-    public E last() {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.last();
-    }
-
-    @Override
-    public TinkerOrderedSet<E> reverse() {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.reverse();
-    }
-
-    @Override
-    public TinkerOrderedSet<E> including(E e) {
-        maybeLoad();
-        return this.oclStdLibOrderedSet.including(e);
-    }
 }
--- runtime/runtime-domain/src/main/java/org/tuml/runtime/collection/persistent/TinkerQualifiedOrderedSetImpl.java
+++ runtime/runtime-domain/src/main/java/org/tuml/runtime/collection/persistent/TinkerQualifiedOrderedSetImpl.java
@@ -3,49 +3,36 @@ package org.tuml.runtime.collection.persistent;
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
-import org.apache.commons.collections.set.ListOrderedSet;
 import org.tuml.runtime.adaptor.GraphDb;
-import org.tuml.runtime.collection.*;
-import org.tuml.runtime.collection.ocl.BodyExpressionEvaluator;
-import org.tuml.runtime.collection.ocl.BooleanExpressionEvaluator;
-import org.tuml.runtime.collection.ocl.OclStdLibOrderedSet;
-import org.tuml.runtime.collection.ocl.OclStdLibOrderedSetImpl;
+import org.tuml.runtime.collection.TinkerQualifiedOrderedSet;
+import org.tuml.runtime.collection.TumlRuntimeProperty;
 import org.tuml.runtime.domain.TumlNode;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.ListIterator;
 
-public class TinkerQualifiedOrderedSetImpl<E> extends BaseCollection<E> implements TinkerQualifiedOrderedSet<E> {
-
-	protected OclStdLibOrderedSet<E> oclStdLibOrderedSet;
+public class TinkerQualifiedOrderedSetImpl<E> extends TumlBaseOrderedSet<E> implements TinkerQualifiedOrderedSet<E> {
 
     @SuppressWarnings("unchecked")
-	public TinkerQualifiedOrderedSetImpl(TumlNode owner, TumlRuntimeProperty runtimeProperty) {
-		super(owner, runtimeProperty);
-		this.internalCollection = new ListOrderedSet();
-		this.oclStdLibOrderedSet = new OclStdLibOrderedSetImpl<E>((ListOrderedSet)this.internalCollection); 
-		this.oclStdLibCollection = this.oclStdLibOrderedSet;
-		this.index = GraphDb.getDb().getIndex(owner.getUid() + INDEX_SEPARATOR + getQualifiedName(), Edge.class);
-		if (this.index == null) {
-			this.index = GraphDb.getDb().createIndex(owner.getUid() + INDEX_SEPARATOR + getQualifiedName(), Edge.class);
-		}
-	}
+    public TinkerQualifiedOrderedSetImpl(TumlNode owner, TumlRuntimeProperty runtimeProperty) {
+        super(owner, runtimeProperty);
+        this.index = GraphDb.getDb().getIndex(owner.getUid() + INDEX_SEPARATOR + getQualifiedName(), Edge.class);
+        if (this.index == null) {
+            this.index = GraphDb.getDb().createIndex(owner.getUid() + INDEX_SEPARATOR + getQualifiedName(), Edge.class);
+        }
+    }
 
-	public ListOrderedSet getInternalListOrderedSet() {
-		return (ListOrderedSet) this.internalCollection;
-	}
-
-	@Override
-	public void add(int indexOf, E e) {
-		maybeLoad();
-		Edge edge = addToListAtIndex(indexOf, e);
-		// Can only qualify TinkerNode's
-		if (!(e instanceof TumlNode)) {
-			throw new IllegalStateException("Primitive properties can not be qualified!");
-		}
-		addQualifierToIndex(edge, (TumlNode)e);
-	}
+    @Override
+    public void add(int indexOf, E e) {
+        maybeLoad();
+        if (!this.getInternalListOrderedSet().contains(e)) {
+            Edge edge = addToListAtIndex(indexOf, e);
+            // Can only qualify TinkerNode's
+            if (!(e instanceof TumlNode)) {
+                throw new IllegalStateException("Primitive properties can not be qualified!");
+            }
+            addQualifierToIndex(edge, (TumlNode) e);
+        }
+    }
 
     @Override
     protected void addToLinkedList(Edge edge) {
@@ -198,36 +185,6 @@ public class TinkerQualifiedOrderedSetImpl<E> extends BaseCollection<E> implemen
         }
     }
 
-//	@Override
-//	public boolean remove(Object o) {
-//		maybeLoad();
-//		int indexOf = this.getInternalListOrderedSet().indexOf(o);
-//		boolean result = this.getInternalListOrderedSet().remove(o);
-//		if (result) {
-//			Vertex v;
-//			if (o instanceof TumlNode) {
-//				TumlNode node = (TumlNode) o;
-//				v = node.getVertex();
-//				Set<Edge> edges = GraphDb.getDb().getEdgesBetween(this.vertex, v, this.getLabel());
-//				for (Edge edge : edges) {
-//					removeEdgefromIndex(v, edge, indexOf);
-//					GraphDb.getDb().removeEdge(edge);
-//				}
-//			} else if (o.getClass().isEnum()) {
-//				v = this.internalVertexMap.get(((Enum<?>) o).name());
-//				Edge edge = v.getEdges(Direction.IN, this.getLabel()).iterator().next();
-//				removeEdgefromIndex(v, edge, indexOf);
-//				GraphDb.getDb().removeVertex(v);
-//			} else {
-//				v = this.internalVertexMap.get(o);
-//				Edge edge = v.getEdges(Direction.IN, this.getLabel()).iterator().next();
-//				removeEdgefromIndex(v, edge, indexOf);
-//				GraphDb.getDb().removeVertex(v);
-//			}
-//		}
-//		return result;
-//	}
-
     @Override
     public boolean remove(Object o) {
         maybeLoad();
@@ -279,166 +236,18 @@ public class TinkerQualifiedOrderedSetImpl<E> extends BaseCollection<E> implemen
     }
 
     @Override
-	public boolean addAll(Collection<? extends E> c) {
-		throw new IllegalStateException("This method can not be called on a qualified association. Call add(E, List<Qualifier>) instead");
-	}
+    public boolean addAll(Collection<? extends E> c) {
+        throw new IllegalStateException("This method can not be called on a qualified association. Call add(E, List<Qualifier>) instead");
+    }
 
-	@Override
-	public void clear() {
-		maybeLoad();
-		for (Object e : this.getInternalListOrderedSet()) {
-			this.remove(e);
-		}
-	}
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        throw new IllegalStateException("This method can not be called on a qualified association. Call add(E, List<Qualifier>) instead");
+    }
 
-	@Override
-	public boolean addAll(int index, Collection<? extends E> c) {
-		throw new IllegalStateException("This method can not be called on a qualified association. Call add(E, List<Qualifier>) instead");
-	}
+    @Override
+    public E set(int index, E element) {
+        throw new IllegalStateException("This method can not be called on a qualified association. Call add(E, List<Qualifier>) instead");
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public E get(int index) {
-		if (!this.loaded) {
-			loadFromVertex();
-		}
-		return (E) this.getInternalListOrderedSet().get(index);
-	}
-
-	@Override
-	public E set(int index, E element) {
-		throw new IllegalStateException("This method can not be called on a qualified association. Call add(E, List<Qualifier>) instead");
-	}
-
-	@Override
-	public E remove(int index) {
-		E e = this.get(index);
-		this.remove(e);
-		return e;
-	}
-
-	@Override
-	public int indexOf(Object o) {
-		maybeLoad();
-		return this.getInternalListOrderedSet().indexOf(o);
-	}
-
-	@Override
-	public int lastIndexOf(Object o) {
-		throw new RuntimeException("Not supported");
-	}
-
-	@Override
-	public ListIterator<E> listIterator() {
-		throw new RuntimeException("Not supported");
-	}
-
-	@Override
-	public ListIterator<E> listIterator(int index) {
-		throw new RuntimeException("Not supported");
-	}
-
-	@Override
-	public List<E> subList(int fromIndex, int toIndex) {
-		throw new RuntimeException("Not supported");
-	}
-
-	protected void validateQualifiedMultiplicity(List<Qualifier> qualifiers) {
-		for (Qualifier qualifier : qualifiers) {
-			if (qualifier.isOne()) {
-				long count = this.index.count(qualifier.getKey(), qualifier.getValue());
-				if (count > 0) {
-					// Add info to exception
-					throw new IllegalStateException("qualifier fails, entry for qualifier already exist");
-				}
-			}
-		}
-	}
-
-	protected void removeEdgefromIndex(Vertex v, Edge edge, int indexOf) {
-		this.index.remove("index", v.getProperty("tinkerIndex"), edge);
-		for (String key : edge.getPropertyKeys()) {
-			if (key.startsWith("index")) {
-				this.index.remove(key, edge.getProperty(key), edge);
-			}
-		}
-	}
-
-	@Override
-	public <R> TinkerSequence<R> collectNested(BodyExpressionEvaluator<R, E> v) {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.collectNested(v);
-	}
-
-	@Override
-	public <T, R> TinkerSequence<T> collect(BodyExpressionEvaluator<R, E> v) {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.collect(v);
-	}
-	
-	@Override
-	public <T2> TinkerSequence<T2> flatten() {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.flatten();
-	}
-
-	@Override
-	public TinkerOrderedSet<E> select(BooleanExpressionEvaluator<E> v) {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.select(v);
-	}
-
-	@Override
-	public TinkerOrderedSet<E> append(E e) {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.append(e);
-	}
-
-	@Override
-	public TinkerOrderedSet<E> prepend(E e) {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.prepend(e);
-	}
-
-	@Override
-	public TinkerOrderedSet<E> insertAt(Integer index, E e) {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.insertAt(index, e);
-	}
-
-	@Override
-	public TinkerOrderedSet<E> subOrderedSet(Integer lower, Integer upper) {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.subOrderedSet(lower, upper);
-	}
-
-	@Override
-	public E at(Integer i) {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.at(i);
-	}
-
-	@Override
-	public E first() {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.first();
-	}
-
-	@Override
-	public E last() {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.last();
-	}
-
-	@Override
-	public TinkerOrderedSet<E> reverse() {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.reverse();
-	}
-
-	@Override
-	public TinkerOrderedSet<E> including(E e) {
-		maybeLoad();
-		return this.oclStdLibOrderedSet.including(e);
-	}
 }
