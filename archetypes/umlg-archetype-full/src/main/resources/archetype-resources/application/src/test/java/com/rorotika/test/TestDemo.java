6590763891130601f4b51843ee3e5289320b4cf2
pieter <pieter.martin@gmail.com>
2014-01-17 20:08:10 +0200

    change to db.drop

--- archetypes/umlg-archetype-full/src/main/resources/archetype-resources/application/src/test/java/com/rorotika/test/TestDemo.java
+++ archetypes/umlg-archetype-full/src/main/resources/archetype-resources/application/src/test/java/com/rorotika/test/TestDemo.java
@@ -1,19 +1,19 @@
 package com.rorotika.test;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.junit.*;
 import org.umlg.Many;
 import org.umlg.One;
 import org.umlg.root.Root;
 import org.umlg.runtime.adaptor.GraphDb;
 import org.umlg.runtime.adaptor.UmlgGraph;
-import org.umlg.runtime.util.UmlgProperties;
+import org.umlg.runtime.adaptor.UmlgGraphManager;
+import org.umlg.runtime.validation.TumlConstraintViolationException;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.logging.LogManager;
 
 /**
  * Date: 2014/01/15
@@ -24,36 +24,59 @@ public class TestDemo {
     private UmlgGraph db;
 
     @Before
-    public void before() throws IOException {
-        File dbDir = new File(UmlgProperties.INSTANCE.getTumlDbLocation());
-        if (dbDir.exists()) {
-            FileUtils.deleteDirectory(dbDir);
-        }
+    public void before() {
         this.db = GraphDb.getDb();
     }
 
     @After
     public void after() {
-        db.shutdown();
+        db.drop();
     }
 
     @Test
     public void testDemo() {
         One one1 = new One(true);
-        one1.setName("one1");
+        //MaxLength validation ensures name's length must be <= 5
+        one1.setName("12345");
+        one1.setDate(new LocalDate());
         Many many1 = new Many(one1);
-        many1.setName("many1");
+        //MinLength validation ensures name's length must be >= 5
+        many1.setName("12345");
+        many1.setDateTime(new DateTime());
         Many many2 = new Many(one1);
-        many2.setName("many2");
+        many2.setName("12345");
+        many2.setDateTime(new DateTime());
         Many many3 = new Many(one1);
-        many3.setName("many3");
+        many3.setName("12345");
+        many3.setDateTime(new DateTime());
         Many many4 = new Many(one1);
-        many4.setName("many4");
+        many4.setName("12345");
+        many4.setDateTime(new DateTime());
         Many many5 = new Many(one1);
-        many5.setName("many5");
+        many5.setName("12345");
+        many5.setDateTime(new DateTime());
         db.commit();
 
         Assert.assertEquals(1, Root.INSTANCE.getOne().size());
         Assert.assertEquals(5, one1.getMany().size());
+    }
+
+    @Test(expected = TumlConstraintViolationException.class)
+    public void testDemoValidation() {
+        try {
+            One one1 = new One(true);
+            //MaxLength validation ensures name's length must be <= 5
+            one1.setName("12345");
+            one1.setDate(new LocalDate());
+            Many many1 = new Many(one1);
+            //MinLength validation ensures name's length must be >= 5
+            many1.setName("1234");
+            many1.setDateTime(new DateTime());
+            db.commit();
+            Assert.assertEquals(1, Root.INSTANCE.getOne().size());
+            Assert.assertEquals(5, one1.getMany().size());
+        } finally {
+            db.rollback();
+        }
     }
 }
