34a538a569079190968f7ff6719cc60c6908dd17
pieter <pieter.martin@gmail.com>
2016-05-24 10:57:50 +0200

    test on hsqldb

--- test/test-generation/src/main/java/org/umlg/test/generation/GenerateTestProjects.java
+++ test/test-generation/src/main/java/org/umlg/test/generation/GenerateTestProjects.java
@@ -1,13 +1,13 @@
 package org.umlg.test.generation;
 
+import org.umlg.generation.JavaGenerator;
+import org.umlg.javageneration.DefaultVisitors;
+import org.umlg.restlet.generation.RestletVisitors;
+
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.logging.LogManager;
-
-import org.umlg.generation.JavaGenerator;
-import org.umlg.javageneration.DefaultVisitors;
-import org.umlg.restlet.generation.RestletVisitors;
 
 public class GenerateTestProjects {
 
@@ -23,18 +23,16 @@ public class GenerateTestProjects {
         }
 		JavaGenerator javaGenerator = new JavaGenerator();
         javaGenerator.generate(new File(args[0] + "/test/blueprints/umlg-test-blueprints/src/main/model/umlg-test.uml"), new File(args[0] + "/test/blueprints/umlg-test-blueprints/"), DefaultVisitors.getDefaultJavaVisitors());
-//        javaGenerator = new JavaGenerator();
-//		javaGenerator.generate(new File(args[0] + "/test/test-restlet/src/main/model/restAndJson.uml"), new File(args[0] + "/test/test-restlet/"), RestletVisitors.getDefaultJavaVisitors());
-//		javaGenerator = new JavaGenerator();
-//		javaGenerator.generate(new File(args[0] + "/test/umlg-test-basic/src/main/model/umlg-test-basic.uml"), new File(args[0] + "/test/umlg-test-basic/"), DefaultVisitors.getDefaultJavaVisitors());
-//		javaGenerator = new JavaGenerator();
-//		javaGenerator.generate(new File(args[0] + "/test/umlg-test-ocl/src/main/model/test-ocl.uml"), new File(args[0] + "/test/umlg-test-ocl/"), DefaultVisitors.getDefaultJavaVisitors());
-//        javaGenerator = new JavaGenerator();
-//        javaGenerator.generate(new File(args[0] + "/test/umlg-test-tinkergraph/src/main/model/tinkergraph.uml"), new File(args[0] + "/test/umlg-test-tinkergraph/"), DefaultVisitors.getDefaultJavaVisitors());
-//
-//
-//        javaGenerator = new JavaGenerator();
-//        javaGenerator.generate(new File(args[0] + "/test/test-model-import/src/main/model/model1.uml"),
-//                new File(args[0] + "/test/test-model-import/"), DefaultVisitors.getDefaultJavaVisitors());
+        javaGenerator = new JavaGenerator();
+		javaGenerator.generate(new File(args[0] + "/test/test-restlet/src/main/model/restAndJson.uml"), new File(args[0] + "/test/test-restlet/"), RestletVisitors.getDefaultJavaVisitors());
+		javaGenerator = new JavaGenerator();
+		javaGenerator.generate(new File(args[0] + "/test/umlg-test-basic/src/main/model/umlg-test-basic.uml"), new File(args[0] + "/test/umlg-test-basic/"), DefaultVisitors.getDefaultJavaVisitors());
+		javaGenerator = new JavaGenerator();
+		javaGenerator.generate(new File(args[0] + "/test/umlg-test-ocl/src/main/model/test-ocl.uml"), new File(args[0] + "/test/umlg-test-ocl/"), DefaultVisitors.getDefaultJavaVisitors());
+        javaGenerator = new JavaGenerator();
+        javaGenerator.generate(new File(args[0] + "/test/umlg-test-tinkergraph/src/main/model/tinkergraph.uml"), new File(args[0] + "/test/umlg-test-tinkergraph/"), DefaultVisitors.getDefaultJavaVisitors());
+        javaGenerator = new JavaGenerator();
+        javaGenerator.generate(new File(args[0] + "/test/test-model-import/src/main/model/model1.uml"),
+                new File(args[0] + "/test/test-model-import/"), DefaultVisitors.getDefaultJavaVisitors());
     }
 }
